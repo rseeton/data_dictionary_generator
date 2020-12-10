0fc9dd162bf47a1cd849ceecfa5f845a9832024e
Richard Seeton <richard@seeton.net>
2020-12-09 22:26:01 -0500

    Clean up items list

--- data_dictionary.py
+++ data_dictionary.py
@@ -3,20 +3,24 @@
 #Pandas read csv:   https://www.shanelynn.ie/python-pandas-read_csv-load-data-from-csv-files/
 #list index & value https://stackoverflow.com/questions/38387529/how-to-iterate-over-pandas-series-generated-from-groupby-size
 #create new pandas  https://stackoverflow.com/questions/20638006/convert-list-of-dictionaries-to-a-pandas-dataframe
-
+#series valuecounts https://www.w3resource.com/pandas/series/series-value_counts.php
 
 import pandas as pd
 import argparse
+
+from utility_functions import check_boolean_column
+
+distribution_reporting_limit_float=5      #If more than X floats, don't report distribution percentages
+distribution_reporting_limit_varchar=10   #If more than X varchars, don't report distribution percentages
 
 data = {} # To accomodate spreadsheets with multiple tabs
 
 parser = argparse.ArgumentParser(description='Generate Datasource Layout from file')
 parser.add_argument('filename', type=str, help='Input dir to be parsed')
-parser.add_argument('delim',  type=str, nargs='?', default=',', help="File Delimiter [comma|semi-colon|tab|pipe|xls] defaults to comma")
-parser.add_argument('header', type=int, nargs='?', default='0', help="Number of header record rows (assumes first row represents column headers")
+parser.add_argument('delim',  type=str, nargs='?', default=',', help='File Delimiter [comma|semi-colon|tab|pipe|xls] defaults to comma')
+parser.add_argument('header', type=int, nargs='?', default='0', help='Number of header record rows (assumes first row represents column headers')
 args = parser.parse_args()
 print(args.filename)
-
 
 if args.delim == '' or args.delim == ',':
     data[args.filename] = pd.read_csv(args.filename, delimiter=',', header=args.header)
@@ -34,20 +38,25 @@ elif args.delim == 'xls':
         for x in xls.sheet_names:
             data[x] = pd.read_excel(xls, x, index_col=None, na_values=['NA'])
 else:
-    print ("Delimiter not recognized")
+    print ('Delimiter not recognized')
     exit()
 
-# Preview the first 5 lines of the loaded data
-for x in data:
-    print(x)
+#data_layout = {'Column_Name','Data_Type','Allow_Nulls','Count','Count_NaN','Max','Min','Values'}
+data_dictionary = pd.DataFrame()
 
+
+for x in data:
+    print('Worksheet Name: {}'.format(x))
     types = data[x].dtypes
-    default_value = ''
     allow_defaults = False
-    a = []
     for column_name, v in types.items():
-        allow_nulls = True
-        print('\tColumn', column_name)
+        value_counts= ''
+        field_max=''
+        field_min=''
+        default_value = ''
+        items_list=[]
+        allow_nulls = False
+        print('\tColumn {}'.format(column_name))
         if v == 'float64':
             data_type='FLOAT'
         elif v == 'int64':
@@ -57,27 +66,41 @@ for x in data:
         else:
             data_type='VARCHAR'
 
-        #If indicator column then default to 'N'
-        if column_name[-4:].upper() == '_IND' or column_name[-4:].upper() == 'FLAG' :
-            allow_defaults = True
-            default_value  = 'N'
+        unique_list = data[x][column_name].unique()
+        if check_boolean_column(unique_list):
+            data_type='BOOLEAN'
         elif column_name[-5:].upper() == '_DATE':
             data_type='DATE'
-        #
+        print('\t\tType: {}'.format(data_type))
+        if (((data_type == 'VARCHAR' ) and (len(data[x][column_name].unique()) <= distribution_reporting_limit_varchar )) or ((data_type == 'FLOAT') and (len(data[x][column_name].unique()) <= distribution_reporting_limit_float)) or (data_type == 'BOOLEAN')):
+            print('\t\tDistribution:')
+            value_counts = data[x][column_name].value_counts(normalize=True, sort=True, ascending=False, bins=None, dropna=False)
+            print(type(value_counts.items()))
+            for item, amount in value_counts.items():  
+                items_list.append(item)
+                print('\t\t\t{} ({:.2f}%)'.format(item, amount*100))
+        else:
+            print('\t\tDistrubution: VARCHAR with count > {} or FLOAT with count > {}, no details shown'.format( distribution_reporting_limit_varchar,  distribution_reporting_limit_float))
+
+        count_total=len(data[x][column_name])
+        count_nan  =data[x][column_name].isna().sum()
+        if count_nan > 0:
+            allow_nulls=True
+        print('\t\tCount        :{}'.format(count_total))
+        print('\t\tCount (NaN)  :{}'.format(count_nan))
+        if data_type not in ('BOOLEAN','VARCHAR'):
+            field_max=data[x][column_name].max()
+            field_min=data[x][column_name].min()
+            print('\t\tMax Value    :{}'.format(field_max))
+            print('\t\tMin Value    :{}'.format(field_min))
         #print ('Field_Name'+column_name,'Data_Type'+data_type,'Allow_Nulls'+str(allow_nulls),'Default_Value'+default_value,'Allow_Defaults'+str(allow_defaults) )
-        a.append({'Field_Name' : column_name,'Data_Type' : data_type,'Allow_Nulls' : allow_nulls,'Default_Value' : default_value,'Allow_Defaults' : allow_defaults}) 
-        print('\t\tType:', data_type)
-        if len(data[x][column_name].unique()) <= 10:
-            print('\t\tUnique: ',data[x][column_name].unique())
-        if data_type !=  "VARCHAR":
-            print('\t\tMax :', data[x][column_name].max())
-            print('\t\tMin :', data[x][column_name].min())
+        new_row = {'Column_Name':column_name, 'Data_Type':data_type, 'Allow_Nulls':allow_nulls,'Count':count_total,'Count_NaN':count_nan,'Max':field_max,'Min':field_min,'Values':items_list}
+        data_dictionary = data_dictionary.append(new_row, ignore_index=True)
 
+    data_dictionary.set_index('Column_Name',inplace=True)
+    print(data_dictionary)
 
-    layout_data = pd.DataFrame(a)
-
-    #Create layout dataframe
-    layout_header = pd.DataFrame(columns=['Field_Name','Description','Title','Data_Type','Size','Allow_Nulls','Allow_Defaults','Default_Value','Synthetic_Content','Lookup_Source','Value_Field','Description_Field','Parent_Fields','Display_Format','Show_Subtotal','Aggregated_Function','Is_Count_Field_For','Is_Hidden','Field_Type','Default_Instance_Key_Value_Formula','Generate_Auto_Unique_Id','Data_Permission_Entity','Join_Source_For_Permissions','Join_Field_For_Permissions'])
-
-    layout=pd.concat([layout_header, layout_data], axis=1, sort=False)
-    #print(layout)
+    html = data_dictionary.to_html()
+    text_file = open("index.html", "w")
+    text_file.write(html)
+    text_file.close()
